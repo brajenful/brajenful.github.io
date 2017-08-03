e8f53011b5a9ec0b0481b3ef3684199ec01a4e29
brajenful <zolee12@indamail.hu>
2017-08-03 16:11:18 +0200

    renamed temperature.html, added weather parameters

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,25 @@
+function getWeather() {
+	var weatherAPIXU = "https://api.apixu.com/v1/current.json?key=595d5646dcb847f8a9e132542170308&q=46.9,19.6";
+	$.getJSON(weatherAPIXU, function(data) {
+		var temp_c = data.current.temp_c;
+		$("#temp_c").html(temp_c);
+		var wind_kph = data.current.wind_kph;
+		$("#wind_kph").html(wind_kph);
+		var wind_dir = data.current.wind_dir;
+		$("#wind_dir").html(wind_dir);
+		var precip_mm = data.current.precip_mm;
+		$("#precip_mm").html(precip_mm);
+		var humidity = data.current.humidity;
+		$("#humidity").html(humidity);
+		var last_updated = data.current.last_updated;
+		$("#last_updated").html(last_updated);
+	});
+}
+
 $(document).ready(function() {
-    $("#update").click(function() {
-    	var weatherAPIXU = "https://api.apixu.com/v1/current.json?key=595d5646dcb847f8a9e132542170308&q=46.9,19.6";
-    	$.getJSON(weatherAPIXU, function(data) {
-        	var forecast = data.current.temp_c;
-        	$("#value").html(forecast);
-    	});
+	getWeather();
+	$("#update").click(function() {
+		getWeather();
     	/*
     	var ref = firebase.database().ref("temp");
     	ref.once("value")
