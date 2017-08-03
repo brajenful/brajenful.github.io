33bb3fc2b299f2cfc50213ebfd841bc43aa02e6c
brajenful <zolee12@indamail.hu>
2017-08-03 17:24:36 +0200

    renamed and modified scripts.js

--- js/scripts-weather.js
+++ js/scripts-weather.js
@@ -1,4 +1,35 @@
+var temp_c;
+var wind_kph;
+var wind_dir;
+var precip_mm;
+var humidity;
+var last_updated;
+var localtime;
+
 function getWeather() {
+	var weatherAPIXU = "https://api.apixu.com/v1/current.json?key=595d5646dcb847f8a9e132542170308&q=46.9,19.6";
+	$.getJSON(weatherAPIXU, function(data) {
+		temp_c = data.current.temp_c;
+		wind_kph = data.current.wind_kph;
+		wind_dir = data.current.wind_dir;
+		precip_mm = data.current.precip_mm;
+		humidity = data.current.humidity;
+		last_updated = data.current.last_updated;
+		localtime = data.location.localtime;
+	});
+}
+
+function updateWeather() {
+	$("#temp_c").html(temp_c);
+	$("#wind_kph").html(wind_kph);
+	$("#wind_dir").html(wind_dir);
+	$("#precip_mm").html(precip_mm);
+	$("#humidity").html(humidity);
+	$("#last_updated").html(last_updated);
+	$("#localtime").html(localtime);
+}
+
+function getupdate() {
 	var weatherAPIXU = "https://api.apixu.com/v1/current.json?key=595d5646dcb847f8a9e132542170308&q=46.9,19.6";
 	$.getJSON(weatherAPIXU, function(data) {
 		var temp_c = data.current.temp_c;
@@ -19,14 +50,15 @@ function getWeather() {
 }
 
 $(document).ready(function() {
-	$(".label, .value").hide(0).delay(200).fadeIn();
-	getWeather();
+	getupdate();
+	$(".label, .value").hide(0).delay(0).fadeIn();
 	$("#update").click(function() {
+		getWeather();
 		$(".label, .value").fadeOut(function() {
-    		getWeather();
+    		updateWeather();
     		setTimeout(function() {
     			$(".label, .value").fadeIn();
-    		}, 100);
+    		}, 0);
 		});
     	/*
     	var ref = firebase.database().ref("temp");
