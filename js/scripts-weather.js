6406473ada18c344deaaae09e2dbfe4759cace88
brajenful <zolee12@indamail.hu>
2017-08-03 19:48:11 +0200

    added search functionality to weather project

--- js/scripts-weather.js
+++ js/scripts-weather.js
@@ -1,3 +1,4 @@
+var location_name;
 var temp_c;
 var wind_kph;
 var wind_dir;
@@ -5,10 +6,11 @@ var precip_mm;
 var humidity;
 var last_updated;
 var localtime;
+var weatherAPIXU;
 
 function getWeather() {
-	var weatherAPIXU = "https://api.apixu.com/v1/current.json?key=595d5646dcb847f8a9e132542170308&q=46.9,19.6";
 	$.getJSON(weatherAPIXU, function(data) {
+		location_name = data.location.name;
 		temp_c = data.current.temp_c;
 		wind_kph = data.current.wind_kph;
 		wind_dir = data.current.wind_dir;
@@ -20,6 +22,7 @@ function getWeather() {
 }
 
 function updateWeather() {
+	$("#location_name").html(location_name);
 	$("#temp_c").html(temp_c);
 	$("#wind_kph").html(wind_kph);
 	$("#wind_dir").html(wind_dir);
@@ -30,8 +33,9 @@ function updateWeather() {
 }
 
 function getupdate() {
-	var weatherAPIXU = "https://api.apixu.com/v1/current.json?key=595d5646dcb847f8a9e132542170308&q=46.9,19.6";
 	$.getJSON(weatherAPIXU, function(data) {
+		var location_name = data.location.name;
+		$("#location_name").html(location_name);
 		var temp_c = data.current.temp_c;
 		$("#temp_c").html(temp_c);
 		var wind_kph = data.current.wind_kph;
@@ -49,27 +53,41 @@ function getupdate() {
 	});
 }
 
-$(document).ready(function() {
-	getupdate();
-	$(".label, .value").hide(0).delay(0).fadeIn();
-	$("#update").click(function() {
-		getWeather();
+function firebaseQuery() {
+	    var ref = firebase.database().ref("temp");
+    	ref.once("value")
+  			.then(function(snapshot) {
+    			var data = snapshot.val();
+    			$("#value").html(data);
+  		});
+}
+
+function updateOnClick() {
+	getWeather();
 		$(".label, .value").fadeOut(function() {
     		updateWeather();
     		setTimeout(function() {
     			$(".label, .value").fadeIn();
     		}, 0);
 		});
-    	/*
-    	var ref = firebase.database().ref("temp");
-    	ref.once("value")
-  			.then(function(snapshot) {
-    			var data = snapshot.val();
-    			$("#value").html(data);
-  		});
-  		*/
+}
+
+$(document).ready(function() {
+	getupdate();
+	$(".label, .value").hide(0).delay(0).fadeIn();
+	$("#update").click(function() {
+		updateOnClick();
     });
+    $("#search input").on('keypress',function (e) {
+   		if (e.keyCode == 13) {
+   			var value = this.value;
+    		weatherAPIXU = "https://api.apixu.com/v1/current.json?key=595d5646dcb847f8a9e132542170308&q=" + value;
+    		updateOnClick();
+   		}
+	});
 });
+
+
 /*
 $(document).ready(function loadData() {
     var weatherAPIXU = "https://api.apixu.com/v1/current.json?key=595d5646dcb847f8a9e132542170308&q=46.9,19.6";
